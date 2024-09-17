e469783b50c19c0f04a50e7ec807334fb58eb668
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2024-09-17 16:04:18 +0000

    add page initials

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+function Home() {
+    return <h1>Você só está aqui pois é parte fundamental da minha vida, obrigado! <br></br><br></br><br></br>
+       
+        Alias, Mariana, eu te amo! se você me ama da uma risadinha!
+    </h1>
+}
+
+export default Home;
