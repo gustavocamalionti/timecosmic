a4acf2fbb2d3c50c8bdde58a92f261b9e3051e1c
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2024-09-19 23:11:03 +0000

    alter name Mariana in page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 function Home() {
     return <h1>Você só está aqui pois é parte fundamental da minha vida, obrigado! <br></br><br></br><br></br>
        
-        Alias, Mariana, eu te amo! se você me ama da uma risadinha!
+        Alias, buchicho, eu te amo! se você me ama da uma risadinha!
     </h1>
 }
 
