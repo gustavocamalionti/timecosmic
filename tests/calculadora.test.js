82214a3676ea8ba4a1e57acbd247d380ecff9bdc
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2024-09-26 20:24:36 +0000

    Include an educational example of how to deploy TDD - Test Driven Development (these files will be deleted in the next commit)

---
+++ tests/calculadora.test.js
@@ -0,0 +1,26 @@
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+    const resultado = calculadora.somar(2, 2);
+    expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 deveria retornar 105", () => {
+    const resultado = calculadora.somar(5, 100);
+    expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Erro'", () => {
+    const resultado = calculadora.somar("banana", 100);
+    expect(resultado).toBe("Erro");
+});
+
+test("somar 4 + 'Banana' deveria retornar 'Erro'", () => {
+    const resultado = calculadora.somar(4, "Banana");
+    expect(resultado).toBe("Erro");
+});
+
+test("somar 'Maça' + 'Banana' deveria retornar 'Erro'", () => {
+    const resultado = calculadora.somar("Maça", "Banana");
+    expect(resultado).toBe("Erro");
+});
