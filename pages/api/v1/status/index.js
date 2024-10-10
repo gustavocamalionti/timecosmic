bf88807a2b7ed7ce9fdec1e28d4ca7d7c0e0363d
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2024-10-10 01:22:25 +0000

    add parameters to return endpoint `status` - resolution 1

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    response
-        .status(200)
-        .json({ message: "alunos do curso.dev são pessoas acima da média" });
+    const updatedAt = new Date().toISOString();
+    const databaseVersion = await database.query("SELECT version() as version");
+    const databaseConnectionsMax = await database.query("SHOW MAX_CONNECTIONS");
+    const databaseConnectionsOpen = await database.query(
+        "select count(*) from pg_stat_activity",
+    );
+
+    response.status(200).json({
+        updated_at: updatedAt,
+        database_version: databaseVersion.rows[0].version,
+        database_connections_max:
+            databaseConnectionsMax.rows[0].max_connections,
+        database_connections_open: databaseConnectionsOpen.rowCount,
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,28 @@
 test("Get to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    expect(responseBody.updated_at).toBeDefined();
+    expect(responseBody.database_version).toBeDefined();
+    expect(responseBody.database_connections_max).toBeDefined();
+    expect(responseBody.database_connections_open).toBeDefined();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    const parsedDatabaseConnectionsMax = new Number(
+        responseBody.database_connections_max,
+    ).toString();
+    const parsedDatabaseConnectionsOpen = parseInt(
+        responseBody.database_connections_open,
+    );
+
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+    expect(responseBody.database_version.includes("PostgreSQL")).toBe(true);
+    expect(responseBody.database_connections_max).toEqual(
+        parsedDatabaseConnectionsMax,
+    );
+    expect(responseBody.database_connections_open).toEqual(
+        parsedDatabaseConnectionsOpen,
+    );
 });
