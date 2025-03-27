b2521839bade9ed4210fa0250eaf83292de3e331
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2025-03-27 09:04:20 +0000

    fix `prettier` style linting

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,16 +3,15 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-    
-        const allowedMethods = ["GET", "POST"];
-        if (!allowedMethods.includes(request.method)) {
-            return response.status(405).json({
-                error: `Method "${request.method}" not allowed`,
-            });
-        }
+    const allowedMethods = ["GET", "POST"];
+    if (!allowedMethods.includes(request.method)) {
+        return response.status(405).json({
+            error: `Method "${request.method}" not allowed`,
+        });
+    }
 
-        let dbClient;
-        try {
+    let dbClient;
+    try {
         dbClient = await database.getNewClient();
 
         const defaultMigrationOptions = {
@@ -44,7 +43,6 @@ export default async function migrations(request, response) {
 
             response.status(200).json(migratedMigrations);
         }
-
     } catch (error) {
         console.error(error);
         throw error;
